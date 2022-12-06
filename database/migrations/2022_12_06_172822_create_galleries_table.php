aa493e47b309d2aa02b3fd2d2bdc57aff2e1a7ea
s-cumpalovic <stefancumpalovic98@gmail.com>
2022-12-06 18:44:56 +0100

    Migrations and Relations between tables

--- database/migrations/2022_12_06_172822_create_galleries_table.php
+++ database/migrations/2022_12_06_172822_create_galleries_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('galleries', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('galleries');
     }
 };
